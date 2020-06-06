d821abf8bf59b7dea9471bda2f100d6b59d508f9
ndvu <ndungvu93@gmail.com>
2020-06-06 20:49:01 +0700

    merge

--- src/main/java/testgit/Test1.java
+++ src/main/java/testgit/Test1.java
@@ -7,6 +7,8 @@ public class Test1 {
 		System.out.println("branch1");
 		System.out.println("branch1");
 		System.out.println("branch1");
+		System.out.println("branch1");
+		System.out.println("branch1");
 	}
 
 }
