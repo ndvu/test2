9c06ca5bb24e842566991bf45cbddb57f60b8422
ndvu <ndungvu93@gmail.com>
2020-06-06 20:33:59 +0700

    branch1 branch1 branch1

--- src/main/java/testgit/Test1.java
+++ src/main/java/testgit/Test1.java
@@ -7,6 +7,9 @@ public class Test1 {
 		System.out.println("branch1");
 		System.out.println("branch1");
 		System.out.println("branch1");
+		System.out.println("branch1");
+		System.out.println("branch1");
+		System.out.println("branch1");
 	}
 
 }
