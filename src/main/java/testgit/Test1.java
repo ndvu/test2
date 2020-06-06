44300a154f0151c325af137af7533a97a5bdb9ac
ndvu <ndungvu93@gmail.com>
2020-06-06 21:00:47 +0700

    This is a combination of 2 commits.  The first commit's message is: 1  This is the 2nd commit message: 2

--- src/main/java/testgit/Test1.java
+++ src/main/java/testgit/Test1.java
@@ -10,6 +10,8 @@ public class Test1 {
 		System.out.println("branch1");
 		System.out.println("branch1");
 		System.out.println("branch1");
+		System.out.println("branch1");
+		System.out.println("branch1");
 	}
 
 }
