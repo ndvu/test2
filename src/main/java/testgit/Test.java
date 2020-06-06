6c6ef1299e85b180e93098401b0cb958fef14a8a
ndvu <ndungvu93@gmail.com>
2020-06-06 20:26:43 +0700

    asd

--- src/main/java/testgit/Test.java
+++ src/main/java/testgit/Test.java
@@ -32,6 +32,8 @@ public class Test {
 		System.out.println("master");
 		System.out.println("master");
 		System.out.println("branch1");
+		System.out.println("branch1");
+		System.out.println("branch1");
 	}
 
 }
