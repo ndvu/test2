32bd5408a2db0e197a9772d8d9c1a5952cbd08b4
ndvu <ndungvu93@gmail.com>
2020-06-06 20:32:15 +0700

    branch1

--- src/main/java/testgit/Test1.java
+++ src/main/java/testgit/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("branch1");
+		System.out.println("branch1");
+		System.out.println("branch1");
 	}
 
 }
