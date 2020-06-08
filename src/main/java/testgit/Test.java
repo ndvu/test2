75574b3696c6b8f46fc3ef844e96010b9c3954ae
ndvu <ndungvu93@gmail.com>
2020-06-08 21:48:04 +0700

    eep-002 eep-002 eep-002

--- src/main/java/testgit/Test.java
+++ src/main/java/testgit/Test.java
@@ -58,6 +58,9 @@ public class Test {
 		System.out.println("master");
 
 		System.out.println("EEP-001");
+		System.out.println("EEP-002");
+		System.out.println("EEP-002");
+		System.out.println("EEP-002");
 	}
 
 }
