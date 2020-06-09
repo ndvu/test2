9c0c647692ec889b40e1742b51ae5eabeabebe69
ndvu <ndungvu93@gmail.com>
2020-06-09 20:46:00 +0700

    eep004 eep-004

--- src/main/java/testgit/Test.java
+++ src/main/java/testgit/Test.java
@@ -66,6 +66,8 @@ public class Test {
 		System.out.println("EEP-002");
 		System.out.println("EEP-003");
 		System.out.println("EEP-004");
+		System.out.println("EEP-004");
+		System.out.println("EEP-004");
 	}
 
 }
